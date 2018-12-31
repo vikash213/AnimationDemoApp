f7ad1ff0bb76a21d25840d0904839ce35c8c654d
Appinventiv <appinventiv@apples-macbook-pro-4.local>
2018-12-31 13:49:16 +0530

    Initial Commit

---
+++ AnimationDemoApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AnimationDemoApp
+//
+//  Created by Appinventiv on 31/12/18.
+//  Copyright © 2018 Vikash. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
